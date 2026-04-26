8d3a3f00ebd11ba05ef57efba1c0392fff478784
Luke Peterson <luke@infoplatter.com>
2026-04-26 01:34:01 -0600

    Pre-starting the baseline number of prolog worker processes

--- lib/prolog_bridge/application.ex
+++ lib/prolog_bridge/application.ex
@@ -6,11 +6,30 @@ defmodule PrologBridge.Application do
     pool_config = [
       name: {:local, :prolog_pool},
       worker_module: PrologBridge.Worker,
+      # The number of worker swipl processes to initially start
       size: 2,
+      # The maximum number of additional swipl processes to start as load requires
       max_overflow: 14
     ]
 
-    children = [:poolboy.child_spec(:prolog_pool, pool_config, [])]
-    Supervisor.start_link(children, strategy: :one_for_one, name: PrologBridge.Supervisor)
+    children = [
+      :poolboy.child_spec(:prolog_pool, pool_config, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: PrologBridge.Supervisor]
+
+    case Supervisor.start_link(children, opts) do
+      {:ok, pid} ->
+        # Initialization Phase:
+        # We ensure the baseline workers are hot by running a dummy query for each.
+        # This blocks Application.start until the KB is loaded in the baseline.
+        1..pool_config[:size]
+        |> Enum.map(fn _ -> Task.async(fn -> PrologBridge.query("is_valid(1)") end) end)
+        |> Task.await_many(60_000)
+
+        {:ok, pid}
+
+      error -> error
+    end
   end
 end
