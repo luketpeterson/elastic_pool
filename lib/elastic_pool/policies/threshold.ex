856a38eacc78162cad5fc5b1f01c97b585db5e6d
Luke Peterson <luke@infoplatter.com>
2026-05-04 07:40:46 -0600

    improving threshold scaling policy to do what it's supposed to do.

--- lib/elastic_pool/policies/threshold.ex
+++ lib/elastic_pool/policies/threshold.ex
@@ -1,14 +1,22 @@
 defmodule ElasticPool.Policies.Threshold do
   @moduledoc """
-  A reactive scaling policy that increases the pool size by 1 worker until there
-  are fewer than `scale_threshold` requests waiting to be serviced
+  A reactive scaling policy that increases the pool size by 1 worker at a time
+  until there are fewer than `scale_up_threshold` requests waiting to be serviced,
+  and decreases the pool size by 1 worker at a time until there are
+  `available_reserve` free workers or fewer.
+
+  This is a simple policy, but it shouldn't be used unless workers are very fast
+  to start and stop.  The reactive nature means it's virtually guaranteed to
+  under-provision workers until latency starts getting bad, and then it will
+  over-shoot and bring up more workers than needed because of the backlog.
   """
   @behaviour ElasticPool.ScalingPolicy
 
   @impl true
   def init(%{policy_opts: opts, pool_config: pool}) do
     %{
-      threshold: opts[:scale_threshold] || 10,
+      available_reserve: opts[:available_reserve] || 4,
+      scale_up_threshold: opts[:scale_up_threshold] || 10,
       max_workers: pool.max_workers,
       baseline_workers: pool.baseline_workers
     }
@@ -16,22 +24,35 @@ defmodule ElasticPool.Policies.Threshold do
 
   @impl true
   def handle_event(event, pool, state) do
-    total = ElasticPool.target_workers(pool)
-    waiting = ElasticPool.waiting_clients(pool)
+    old_target = ElasticPool.target_workers(pool)
+    active = ElasticPool.active_workers(pool)
 
     target =
-      case event do
-        :checkout_failed ->
-          # Calculate how many extra 'threshold-sized' blocks we need
-          extra = Float.ceil(waiting / state.threshold) |> trunc()
-          min(state.max_workers, total + extra)
+      if active == old_target do
+        # We're in a steady state, so we might want to adjust the target
+        case event do
+          :checkout_failed ->
+            waiting = ElasticPool.waiting_clients(pool)
+            if waiting > state.scale_up_threshold do
+              old_target + 1
+            else
+              old_target
+            end
 
-        :checkin ->
-          # If no one is waiting, we can eventually drift to baseline
-          if waiting == 0, do: state.baseline_workers, else: total
+          :checkin ->
+            available = ElasticPool.available_workers(pool)
+            if available > state.available_reserve do
+              old_target - 1
+            else
+              old_target
+            end
 
-        _ ->
-          total
+          _ ->
+            old_target
+        end
+      else
+        # A worker is starting or stopping, so let's just let it be until that finshes
+        old_target
       end
 
     {max(target, state.baseline_workers), state}
